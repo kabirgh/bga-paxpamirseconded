f555dc3d8aedf58e65db0c1a7933e0d12b067959
Plod <kabirk@live.com>
2021-10-28 22:49:49 +0800

    Move js file into js folder

--- modules/js/paxpamir-ui.js
+++ modules/js/paxpamir-ui.js
@@ -1,8 +1,8 @@
 define(["dojo", "dojo/_base/declare"], (dojo, declare) => {
   return declare("paxpamir.ui", null, {
-    changeLayoutViewMode: function(event) {
+    changeLayoutViewMode: function (event) {
       const element = dojo.byId(event.target.id);
-      const targetSection = event.target.id.split('-')[1] 
+      const targetSection = event.target.id.split('-')[1]
       dojo.toggleClass(element, 'disabled-layout-action-button');
       dojo.toggleClass(`paxpamir-${targetSection}`, 'section-visible');
     },
