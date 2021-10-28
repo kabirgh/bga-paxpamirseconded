41eda5ad21b94c3e5bcf0f78b4c1c3062335ff8e
Plod <kabirk@live.com>
2021-10-28 20:54:28 +0800

    Autload classes

--- paxpamirseconded.game.php
+++ paxpamirseconded.game.php
@@ -17,12 +17,21 @@
  *
  */
 
+$swdNamespaceAutoload = function ($class) {
+    $classParts = explode('\\', $class);
+    if ($classParts[0] == 'PAX') {
+        array_shift($classParts);
+        $file = dirname(__FILE__) . '/modules/php/' . implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            var_dump('Cannot find file : ' . $file);
+        }
+    }
+};
+spl_autoload_register($swdNamespaceAutoload, true, true);
 
 require_once(APP_GAMEMODULE_PATH . 'module/table/table.game.php');
-// require_once('php/core/Game.php');
-// require_once('php/model/DbModel.php');
-// require_once('php/model/Player.php');
-
 
 class PaxPamirSecondEd extends Table
 {
